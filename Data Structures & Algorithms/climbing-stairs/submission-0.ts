c46720f87cf0e331d2cf31303cd546086f486342
hello_khoa <118266027+Khoacoder22@users.noreply.github.com>
2026-08-04 23:29:03 +0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    // giống bài toán có bao nhiêu cách leo lên 
+    climbStairs(n: number): number {
+        if(n <= 2){
+            return n;
+        }
+
+        let prev2 = 1;
+        let prev1 = 2;
+
+        for(let i=3; i <= n; i++){
+            const current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+}
